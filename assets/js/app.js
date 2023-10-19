4b9a368cc676d428800fa6a36c50b2ad2b57ad2e
Gabriel Rocha <gabriel.rocha20@live.com>
2023-10-19 12:06:12 -0300

    created menu mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,19 @@
-let menuHamburguer = document.getElementById('menu-hamburguer')
-menuHamburguer.addEventListener('click', () => {
-    console.log('Cliquei')
-})
+// Pegando os elementos
+let botaoHamburguer = document.getElementById("header__menu-hamburguer--botao")
+let botaoFechar = document.getElementById("botaoFechar")
+let navbar = document.getElementById("header__menu-hamburguer--nav")
+
+// Função para mostrar o menu
+function mostraMenuHamburguer() {
+    // Verificando se o navbar está ativo ou inativo e trocando as classes
+    if (navbar.classList.contains("header__menu-hamburguer--inactive")) {
+        navbar.classList.remove("header__menu-hamburguer--inactive")
+        navbar.classList.add("header__menu-hamburguer--active")
+    } else if (navbar.classList.contains("header__menu-hamburguer--active")) {
+        navbar.classList.remove("header__menu-hamburguer--active")
+        navbar.classList.add("header__menu-hamburguer--inactive")
+    }
+}
+
+botaoFechar.addEventListener('click', mostraMenuHamburguer)
+botaoHamburguer.addEventListener('click', mostraMenuHamburguer)
